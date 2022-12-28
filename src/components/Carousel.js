bf447bd2947bcc4d14078ca8a4b7b063d9e41bde
Praduman03 <praduman03k@gmail.com>
2022-12-28 16:48:54 +0530

    task done

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -8,6 +8,37 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // complete the function below:
 function Carousel() {
+    const [count,Setcount]=useState(0)
+
+    const back=()=>{
+        if(count===0){
+            Setcount(2)
+        }
+        else{
+            Setcount(count-1)
+        }
+    }
+    const front=()=>{
+        if(count===2){
+            Setcount(count*0)
+        }
+        else{
+            Setcount(count+1)
+        }
+    }
+    return(
+        <div>
+            <div className="container">
+                <ArrowBackIosIcon onClick={back} className="arrowback"/>
+                    <div className="imgCard">
+                        <h2>{images[count].title}</h2>
+                        <h2>{images[count].subtitle}</h2>
+                        <img src={images[count].img} alt="" />
+                    </div>
+                <ArrowForwardIosIcon onClick={front} className="arrowfront"/>
+            </div>
+        </div>
+    )
 }
 
 export default Carousel;
